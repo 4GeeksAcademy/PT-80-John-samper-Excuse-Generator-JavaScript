e0375dfc161ab7c3c21429c7bef29a37a40a4980
John Samper <johnsamperse@gmail.com>
2025-04-24 00:03:04 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird', 'Mr,clean'];
+let action = ['ate', 'peed', 'crushed', 'broke','vandalized'];
+let what = ['my homework', 'my phone', 'the car',];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+function GetRandomNumber (min,max){
+  return Math.floor(Math.random() * (max - min) + min );
+}
+function getRandom(anyArray){
+  let max = anyArray.length -0;
+  let min = 0;
+  let random = GetRandomNumber(min,max)
+  return anyArray [random];
+}
+
 window.onload = function() {
   //write your code here
+
+  document.querySelector(`#excuse`).innerHTML =  getRandom(who) + " " + getRandom(action) + " " + getRandom(what)  + " " + getRandom(when)
+
   console.log("Hello Rigo from the console!");
 };
+
+
